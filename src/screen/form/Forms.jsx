036e2a2c9fc5c3b163959fd7350dd7db3b17702a
zahrabola <95353397+zahrabola@users.noreply.github.com>
2023-01-25 14:52:36 +0000

    Update form

--- src/screen/form/Forms.jsx
+++ src/screen/form/Forms.jsx
@@ -1,9 +1,10 @@
 /* formik docs*/
 import Header from "../../Components/Header";
-import { Box, Typography, TextField } from "@mui/material";
+import { Box, Typography, TextField, Button } from "@mui/material";
 import { Formik } from "formik";
 import * as yup from "yup";
 import useMediaQuery from "@mui/material/useMediaQuery";
+import { Form } from "react-router-dom";
 
 /* strings */
 const initialValues ={
@@ -19,8 +20,11 @@ const userCheckoutSchema = yup.object().shape({
   firstName: yup.string().required("required"),
   lastName: yup.string().required("required"),
   email: yup.string().email("invalid email").required("required"),
-  contact:yup.string().matches(phoneRegExp, "Phone number is not valid"),
-  address1: yup.string().required("required").required("required"),
+  contact:yup
+   .string()
+   .matches(phoneRegExp, "Phone number is not valid")
+   .required("required"),
+  address1: yup.string().required("required"),
   address2: yup.string().required("required"),
 });
 
@@ -28,17 +32,82 @@ const phoneRegExp =   /^((\+[1-9]{1,4}[ -]?)|(\([0-9]{2,3}\)[ -]?)|([0-9]{2,4})[
 
 const Forms = () => {
   const isnonMobile = useMediaQuery("(min-width:600px)") /* minmum width - for media query in react */
-  const handleSubmitForm = (values) => {
-console.log(values)
+  const handleSubmit = (values) => {
+  console.log(values)
   } //function 
   return (
        <Box m="20px">
-  <Header title="Create User" subtitle="Create a new user profile" />
-  <Formik
-  onSubmit={handleSubmitForm}
-  initialValues={initialValues } /* info for the form  */
-  validationSchema={userCheckoutSchema} /* for validation */
-  >
+         <Header title="Create User" subtitle="Create a new user profile" />
+          <Formik
+            onSubmit={handleSubmit}
+            initialValues={initialValues } /* info for the form  */
+            validationSchema={userCheckoutSchema} /* for validation */
+           >{/*pre made values */}
+
+{({
+  values,
+  errors,
+  touched,
+  handleSubmit,
+  handleBlur,
+  handleChange,
+
+}) => (
+  <Form onSubmit={handleSubmit}> 
+<Box 
+display="grid"
+gap="30px"
+gridTemplateColumns="repeat(4, minmax(0, 1fr))"
+sx={{
+  "& > div":  { gridColumn:  isnonMobile ? undefined : "span 4" },
+}}
+>
+<TextField
+fullWidth
+variant="filled"
+type="text"
+label="First Name"
+onBlur={handleBlur}
+onChange={handleChange}
+value={values.firstName}
+name="firstName"
+error={!!touched.firstName && !!errors.firstName}
+helperText={touched.firstName && errors.firstName}
+sx={{ gridColumn: "span 2" }}
+/>
+<TextField
+fullWidth
+variant="filled"
+type="text"
+label="Last Name"
+onBlur={handleBlur}
+onChange={handleChange}
+value={values.lastName}
+name="lastName"
+error={!!touched.lastName && !!errors.lastName}
+helperText={touched.lastName && errors.lastName}
+sx={{ gridColumn: "span 2" }} 
+/>
+<TextField />
+<TextField />
+<TextField />
+<TextField />
+</Box>
+<Box>
+  <Button>
+    Create New user
+  </Button>
+</Box>
+  </Form>
+)}
+
+
+
+
+
+
+
+
 
   </Formik>
 
