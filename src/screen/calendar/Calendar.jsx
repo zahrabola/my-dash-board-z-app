40dcf55d06f9a9b5698d9941bbbd53ba60f4f67e
zahrabola <95353397+zahrabola@users.noreply.github.com>
2023-01-26 12:37:14 +0000

    Update calendar

--- src/screen/calendar/Calendar.jsx
+++ src/screen/calendar/Calendar.jsx
@@ -6,11 +6,46 @@ import dayGridPlugin from "@fullcalendar/daygrid";
 import timeGridPlugin from "@fullcalendar/timegrid";
 import interactionPlugin from "@fullcalendar/interaction";
 import listPlugin from "@fullcalendar/list";
+import { useState } from "react";
 const Calendar = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+
+  const [currentevent, setCurrentEvents]= useState([])
+
+  const handleClickData= (selected) => {
+    const title = prompt("Please enter a new title for your event") /* promnt is js function alert popup */
+    const calendarApi = selected.view.calendar;
+    calendarApi.unselect(); 
+
+    if(title){
+      calendarApi.addEvent({
+        id:`${selected.dateStr}-${title}`,
+        title,
+        start:selected.startStr,
+        end: selected.endStr,
+        allDay: selected.allDay
+      })
+    }
+  }
+   const handleClickEvent = (selected) => {
+    if (
+      window.confirm(
+        `Are you sure you want to delete the event '${selected.event.title}'`
+      )
+    ) {
+      selected.event.remove();
+    }
+   }
+
+
+
     return (
-        <div>
-      Calendar
-        </div>
+      <Box m="20px">
+          <Header title="Calendar" subtitle="Full calendar interactive page" />
+
+
+      </Box>
     )
 }
 
