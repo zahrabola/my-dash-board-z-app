458ec02ba990c2e851200cef4c67cedbc9e7cb4e
zahrabola <95353397+zahrabola@users.noreply.github.com>
2023-02-02 14:30:01 +0000

    Update dashboard

--- src/screen/dashboard/Dashboard.jsx
+++ src/screen/dashboard/Dashboard.jsx
@@ -1,7 +1,15 @@
 import Header from "../../Components/Header";
-import { Box, Button,/* IconButton, Typography, */ useTheme } from "@mui/material";
+import { Box, Button,IconButton, Typography, useTheme } from "@mui/material";
 import {tokens} from "../../themes";
-/* mockTransactions  */
+import { mockTransactions  as data } from "../data/DataMock";
+import DownloadOutlinedIcon from "@mui/icons-material/DownloadOutlined";
+import EmailIcon from "@mui/icons-material/Email";
+import PointOfSaleIcon from "@mui/icons-material/PointOfSale";
+import PersonAddIcon from "@mui/icons-material/PersonAdd";
+import TrafficIcon from "@mui/icons-material/Traffic";
+import StatisticsBox from "../../Components/StatisticsBox";
+
+
 const Dashboard = () => {
 
     const theme = useTheme();
@@ -9,21 +17,47 @@ const Dashboard = () => {
     return (
         /*Header */
         <Box m="20px">
-            <Box display="flex" justifyContent="space-between" alignItems="center">
-            <Header title="Dashboard" subtitle="Welcome, to your Dashboard page."/>
+          <Box display="flex" justifyContent="space-between" alignItems="center">
+           <Header title="Dashboard" subtitle="Welcome, to your Dashboard page."/>
             <Box>
-            <Button
-            sx={{
-              backgroundColor: colors.darkredAccent[700],
+             <Button
+              sx={{
+              backgroundColor: colors.blueAccent[700],
               color: colors.grey[100],
               fontSize: "14px",
               fontWeight: "bold",
               padding: "10px 20px",
             }}
-          > </Button>
-            </Box>
-            </Box>
+          >
+            <DownloadOutlinedIcon sx={{ mr: "10px" }} />
+            Download Reports
+          </Button>
         </Box>
+        {/* Grid and charts */}
+
+        <Box>
+                  {/* Row 1 */}
+          <Box>
+            <StatisticsBox />
+            <EmailIcon />
+          </Box>
+          <Box>
+            <StatisticsBox />
+            <PointOfSaleIcon />
+          </Box>
+          <Box>
+            <StatisticsBox />
+            <PersonAddIcon/>
+          </Box>
+          <Box>
+            <StatisticsBox />
+            <TrafficIcon />
+          </Box>
+              {/* Row 2 */}
+
+        </Box>
+      </Box>  
+    </Box>
     )
 }
 
